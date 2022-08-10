ec5321a7249438dd9aa915df7436bee557d09ab3
CUTY_LAB <cuty_lab@CUTY-LABdeMacBook-Air.local>
2022-08-10 13:58:50 +0800

    add carousel

--- flask/router.py
+++ flask/router.py
@@ -5,6 +5,12 @@ app=Flask(__name__)
 
 bootstrap = Bootstrap5(app)
 
+classified_list = [
+	{'filename':"ocean.jpg",'Title':"海岸遊憩與日常生活(Shoreline and recreational activities)"},
+	{'filename':"ocean.jpg",'Title':"海上活動與船隻(Ocean/Waterway Activities)"},
+	{'filename':"ocean.jpg",'Title':"抽煙相關行為(Smoking-related activities)"}
+	]
+
 @app.route('/')
 def index():
 	return render_template('index.html')
@@ -15,10 +21,15 @@ def intro():
 
 @app.route('/classified')
 def classified():
-	return render_template('classified.html')
+	return render_template('classified.html', classified_list = classified_list)
 
 @app.route('/rank')
 def rank():
 	return render_template('rank.html')
+
+@app.route('/test')
+def test():
+	return render_template('test.html')
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',port='5000',debug=True)
+	app.run(host='0.0.0.0',port='8080',debug=True)
