05fe1610e177bf58ce8b7b0e24424c7fd15c3b84
CUTY_LAB <cuty_lab@CUTY-LABdeMacBook-Air.local>
2022-08-11 15:12:09 +0800

    limit upload file type

--- flask/router.py
+++ flask/router.py
@@ -48,6 +48,7 @@ def action():
 @app.route('/404')
 def error():
 	return render_template('404.html')
+
 # @app.errorhandler(404)
 # def page_not_found(e):
 #     return render_template('404.html'), 404
