d9fcf8cd4614b9b081ed4f8525aac32e10370252
Bob Ho <bohanh4@uci.edu>
2022-08-24 14:31:46 +0800

    Added credibility and updated pickle file

--- flask/coast_image.py
+++ flask/coast_image.py
@@ -1,11 +1,12 @@
 class CoastImage:
-    def __init__(self, time, location, imgName, result, GPS=None, desc=None):
+    def __init__(self, time, location, imgName, result={"score":0, "objects":None}, GPS=None, desc=None):
         self.__time = time
         self.__location = location
         self.__imgName = imgName
         self.__result = result
         self.__GPS = GPS
         self.__desc = desc
+        self.__credibility = 1
 
     def getTimeStamp(self):
         return self.__time
@@ -25,6 +26,15 @@ class CoastImage:
     def getDesc(self):
         return self.__desc
 
+    def getCredibility(self):
+        return self.__credibility
+
     def setResult(self, newResult):
         self.__result = newResult
+
+    def setScore(self, newScore):
+        self.__result["score"] = newScore
+
+    def setCredibility(self, newCredibility):
+        self.__credibility = newCredibility
   
